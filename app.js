9090a1899c5c4aa46b575cff26ef997ff3f35d66
rotemx <rotemx@gmail.com>
2015-08-21 11:51:38 +0300

    added express JS + public dir

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Workshop on 8/21/2015.
+ */
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/hello', function (req, res) {
+    res.sendFile( __dirname +  '/public/file2.html');
+});
+
+
+var server = app.listen(80, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
